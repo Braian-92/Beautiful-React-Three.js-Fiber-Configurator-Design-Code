4ba328148b237d98a9634958b0db5c6a79f44b9d
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-13 20:18:20 -0300

    generar luces y sombras en la escena

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Beautiful-React-Three.js-Fiber-Configurator-Design-Code
+
+# generador de codigo para importar glb en react
+[gltf.pmnd.rs](https://gltf.pmnd.rs/)
--- curso_base/src/Canvas.js
+++ curso_base/src/Canvas.js
@@ -1,5 +1,13 @@
 import { Canvas } from '@react-three/fiber'
-import { Environment, Center, OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
+import { 
+  Environment,
+  Center,
+  OrbitControls,
+  useGLTF,
+  AccumulativeShadows,
+  RandomizedLight 
+} from '@react-three/drei'
 
 export const App = ({ position = [-1, 0, 2.5], fov = 25 }) => (
   <Canvas
@@ -12,16 +20,45 @@ export const App = ({ position = [-1, 0, 2.5], fov = 25 }) => (
 
     <Center>
       <Shirt />
+      <Backdrop />
     </Center>
     <OrbitControls />
   </Canvas>
 )
 
-function Shirt() {
+function Shirt(props) {
+  const { nodes, materials } = useGLTF('/shirt_baked_collapsed.glb')
   return (
-    <mesh>
-      <boxGeometry args={[0.5, 0.5, 0.5]} />
-      <meshNormalMaterial />
-    </mesh>
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+      />
+    </group>
   )
 }
+
+function Backdrop() {
+  return (
+    <AccumulativeShadows
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+    >
+      <RandomizedLight 
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+    </AccumulativeShadows>
+  )
+}
+
+useGLTF.preload('/shirt_baked_collapsed.glb');
