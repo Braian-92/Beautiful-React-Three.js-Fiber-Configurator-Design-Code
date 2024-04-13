21cc51062c35719ecbf780f2249df7623aabca02
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-13 19:35:45 -0300

    escena básica

--- curso_base/src/Canvas.js
+++ curso_base/src/Canvas.js
@@ -1,8 +1,27 @@
-export function App() {
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center, OrbitControls } from '@react-three/drei'
+
+export const App = ({ position = [-1, 0, 2.5], fov = 25 }) => (
+  <Canvas
+    shadows
+    camera={{ position, fov }}
+    eventSource={document.getElementById('root')}
+    eventPrefix="client">
+    <ambientLight intensity={0.5} />
+    <Environment preset="city" />
+
+    <Center>
+      <Shirt />
+    </Center>
+    <OrbitControls />
+  </Canvas>
+)
+
+function Shirt() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <mesh>
+      <boxGeometry args={[0.5, 0.5, 0.5]} />
+      <meshNormalMaterial />
+    </mesh>
   )
 }
--- curso_base/src/index.js
+++ curso_base/src/index.js
@@ -1,9 +1,9 @@
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import { App as Canvas } from './Canvas'
 
-import { App as Canvas } from "./Canvas";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<Canvas />);
+createRoot(document.getElementById('root')).render(
+  <>
+    <Canvas />
+  </>
+)
