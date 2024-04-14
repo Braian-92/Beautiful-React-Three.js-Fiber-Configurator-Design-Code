5f05178e97718228d96f1ca2ed960bdfe848bd5a
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-13 22:03:57 -0300

    base para integrar iconos en overlay (estable reversiva)

---
+++ curso_base/src/Overlay.js
@@ -0,0 +1,20 @@
+// import { Logo } from '@pmndrs/branding'
+// npm install @pmndrs/branding
+// import { AiOutlineHighlight, AiOutlineShopping } from 'react-icons/ai'
+// npm install react-icons/ai
+// -- react-icons/ai (requirio validar con la cuenta de git)
+export default function Overlay() {
+  return <Intro />
+}
+
+
+function Intro() {
+  return (
+    <div className="container">
+      <header>
+        {/* <Logo with="40" height="40" /> */}
+        {/* <AiOutlineShopping size="3em"/> */}
+      </header>
+    </div>
+  )
+}
