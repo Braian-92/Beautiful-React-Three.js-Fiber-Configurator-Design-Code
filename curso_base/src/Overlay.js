89a85943cf0b728c50f34d4199054bd03b2b5a21
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-14 14:01:58 -0300

    funcionalidades valtio memorizar estado de pagina, retoques en titulares Overlay

--- curso_base/src/Overlay.js
+++ curso_base/src/Overlay.js
@@ -5,8 +5,13 @@ import { AiOutlineHighlight, AiOutlineShopping, AiFillCamera, AiOutlineArrowLeft
 // npm install react-icons --save (utilizar este metodo)
 // -- react-icons/ai (requirio validar con la cuenta de git)
 
+import { useSnapshot } from 'valtio'
+import { state } from './store'
+
 
 export default function Overlay() {
+  const snap = useSnapshot(state)
+
   return (
     <div className="container">
       <header>
@@ -16,54 +21,39 @@ export default function Overlay() {
         </div>
       </header>
 
-      <Customizer />
+      {snap.intro ? <Intro /> : <Customizer />}
     </div>
   )
 }
 
 function Intro() {
   return (
-    <div className="container">
-      <header>
-        <Logo width="40" height="40" />
+    <section key="main">
+      <div className="section--container">
         <div>
-          <AiOutlineShopping size="3em" />
+          <h1>LET'S DO IT.</h1>
         </div>
-      </header>
-
-      <section key="main">
-        <div className="section--container">
+        <div className="support--content">
           <div>
-            <h1>LET'S DO IT.</h1>
-          </div>
-          <div className="support--content">
-            <div>
-              <p>
-                Create your unique and exclusive shirt with our brand-new 3D
-                customization tool. <strong>Unleash your imagination</strong>{' '}
-                and define your own style.
-              </p>
-              <button style={{ background: 'black' }}>
-                CUSTOMIZE IT <AiOutlineHighlight size="1.3em" />
-              </button>
-            </div>
+            <p>
+              Create your unique and exclusive shirt with our brand-new 3D
+              customization tool. <strong>Unleash your imagination</strong> and
+              define your own style.
+            </p>
+            <button
+              style={{ background: 'black' }}
+              onClick={() => (state.intro = false)}>
+              CUSTOMIZE IT <AiOutlineHighlight size="1.3em" />
+            </button>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   )
 }
 
 function Customizer() {
-  const colors = [
-    '#ccc',
-    '#EFBD4E',
-    '#80C670',
-    '#726DE8',
-    '#EF674E',
-    '#353934',
-    'Purple'
-  ]
+  const colors = ['#ccc', '#EFBD4E', '#80C670', '#726DE8', '#EF674E', '#353934']
   const decals = ['react', 'three2', 'pmndrs']
 
   return (
@@ -90,7 +80,10 @@ function Customizer() {
           DOWNLOAD
           <AiFillCamera size="1.3em" />
         </button>
-        <button className="exit" style={{ background: 'black' }}>
+        <button
+          className="exit"
+          style={{ background: 'black' }}
+          onClick={() => (state.intro = true)}>
           GO BACK
           <AiOutlineArrowLeft size="1.3em" />
         </button>
